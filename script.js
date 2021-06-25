24172c2e4c2f06da0d0a556496f68de5c04ab5bc
kellymgerstner <81729298+kellymgerstner@users.noreply.github.com>
2021-06-25 13:40:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,27 @@
 
 window.addEventListener("load", function(){
     console.log("Page loaded");
-    let form = document.querySelector("form");
+    
+   
+ let form = document.querySelector("form");
     form.addEventListener("submit", function(event){
-        let pilotName = document.querySelectorf("input[name = pilotName]");
-        let copilotName = document.querySelector("input[name = copilotName");
-        let fuelLevel = document.querySelector("input[name = fuelLevel]");
-        let cargoMass = document.querySelector("input[nme = cargoMass");
-
-        if (pilotName ==="" || copilotName === "" || fuelLevel === "" || cargoMass ===""){
-            alert("All fields are required.");
             event.preventDefault();
+        console.log("Form submission cancelled"
         }
     });
 });
 
+function submitForm(){
+ let pilotName = document.querySelectorf("input[name = pilotName]");
+ let copilotName = document.querySelector("input[name = copilotName");
+ let fuelLevel = document.querySelector("input[name = fuelLevel]");
+ let cargoMass = document.querySelector("input[nme = cargoMass");
+
+    if (pilotName ==="" || copilotName === "" || fuelLevel === "" || cargoMass ===""){
+        alert("All fields are required.");
+    }
+ }            
+            
 // /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 // <h2>Mission Destination</h2>
 // <ol>
